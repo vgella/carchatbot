ac6090a60ecba93e483d8c1f5eb6bfc34cb3bf13
vgella <36316634+vgella@users.noreply.github.com>
2024-12-22 09:57:31 -0800

    Update trial_2.py

--- trial_2.py
+++ trial_2.py
@@ -10,7 +10,7 @@ from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMP
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from transformers import AutoTokenizer, AutoModel
-import os; os.environ["OPENAI_API_KEY"] = "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"
+#import os; os.environ["OPENAI_API_KEY"] = "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"
 
 faiss_model = "sentence-transformers/all-MiniLM-L6-v2"
 tokenizer = AutoTokenizer.from_pretrained(faiss_model)
@@ -124,7 +124,7 @@ CAR_TEMPLATE = PromptTemplate(
 )
 
 
-openai_api_key="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"
+openai_api_key=st.secrets["OPENAI_API_KEY"]
 if True:
     # Create an OpenAI instance
     llm = ChatOpenAI(temperature=0,
